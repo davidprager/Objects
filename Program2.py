a1881be917d54975d23d229be48b2377d497dbaa
davidprager <david_prager@yahoo.com>
2023-09-07 22:30:00 -0400

    Initial commit

---
+++ Program2.py
@@ -0,0 +1,8 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #11, Program2
+Your name here
+"""
+
+# This program simulates the creation of a Smartphone and the management of its apps.
+
---
+++ gumball.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #11, Gumball class
+Your name here
+"""
+
+# This module contains the class for a Gumball Machine
